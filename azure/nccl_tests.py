aed250e6bf3bb119e59f6ab332c82c676b25761b
Isayah Reed <isayah.reed@gmail.com>
2022-12-07 02:07:37 -0800

    Update nccl_tests.py
    stable version

--- azure/nccl_tests.py
+++ azure/nccl_tests.py
@@ -12,6 +12,13 @@ import re
 import subprocess
 from reframe.core.backends import getlauncher
 
+#Author: Isayah Reed
+#
+#Purpose: Downloads NCCL and NCCL test repo, builds them, then runs various
+#   NCCL tests on multiple gpus.
+#
+#Example run command: $> ./bin/reframe -C azure_nhc/config/azure_ubuntu_20.py -c nccl-tests.py -r --system nvads_a10_v5:gpu
+
 class fetch_nccl(rfm.RunOnlyRegressionTest):
    descr = 'Fetch NCCL'
    #valid_systems = ['*']
@@ -41,7 +48,7 @@ class fetch_nccl_tests(rfm.RunOnlyRegressionTest):
 
 class build_nccl(rfm.CompileOnlyRegressionTest):
    descr = 'Build NCCL'
-   build_system = 'Autotools'
+   build_system = 'Make'
    build_prefix = variable(str)
    nccl_zip = fixture(fetch_nccl, scope='session')
 
@@ -49,7 +56,7 @@ class build_nccl(rfm.CompileOnlyRegressionTest):
    def prepare_build(self):
       #tarball = f'master.zip'
       tarball = f'nccl.zip'
-      self.build_prefix = f'nccl-master' 
+      self.build_prefix = f'nccl-master'
 
       fullpath = os.path.join(self.nccl_zip.stagedir, tarball)
       self.prebuild_cmds = [
@@ -60,9 +67,10 @@ class build_nccl(rfm.CompileOnlyRegressionTest):
       #arch for A100; change for other GPUs
       ARCH='compute_80'
       CODE='sm_80'
-      gencode="-gencode=arch={},code={}".format(ARCH,CODE)
+      gencode="\"-gencode=arch={},code={}\"".format(ARCH,CODE)
       # make -j src.build NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
-      self.build_system.max_concurrency = 'src.build NVCC_GENCODE=\"{}\"'.format(gencode)
+      self.build_system.max_concurrency=8
+      self.build_system.options=['src.build', "NVCC_GENCODE={}".format(gencode)]
 
    @sanity_function
    def validate_build(self):
@@ -72,7 +80,7 @@ class build_nccl(rfm.CompileOnlyRegressionTest):
 
 class build_nccl_tests(rfm.CompileOnlyRegressionTest):
    descr = 'Build NCCL tests'
-   build_system = 'Autotools'
+   build_system = 'Make'
    build_prefix = variable(str)
    nccl_dir = fixture(build_nccl, scope='environment')
    nccl_tests_zip = fixture(fetch_nccl_tests, scope='session')
@@ -100,16 +108,17 @@ class build_nccl_tests(rfm.CompileOnlyRegressionTest):
             print("Cannot load cannot find nvcc.\n")
             return False
          else:
-            CUDA_HOME = output.stdout[:9] #remove '/bin/nvcc' 
+            CUDA_HOME = output.stdout[:9] #remove '/bin/nvcc'
 
       #find MPI
       MPI='MPI=0'
       MPI_HOME='/opt/openmpi'
       output = util.osext.run_command('which mpicc')
       if output.returncode != 0:
-         #using impi because ompi has different paths/versions for 
+         #using impi because ompi has different paths/versions for
          #  different OS flavors
          output = util.osext.run_command('module load mpi/impi')
+         output = util.osext.run_command('module load mpi/hpcx')
          if output.returncode != 0:
             print("Cannot load modules for MPI.\n")
             MPI='MPI=1'
@@ -121,9 +130,11 @@ class build_nccl_tests(rfm.CompileOnlyRegressionTest):
          MPI_HOME = output.stdout[:10] #remove '/bin/mpicc'
 
 
-      #build command: $> make MPI=1 MPI_HOME=/path/to/mpi 
-      #     CUDA_HOME=/path/to/cuda NCCL_HOME=/path/to/nccl 
-      self.build_system.max_concurrency = "{} MPI_HOME={} CUDA_HOME={} NCCL_HOME={}".format(MPI,MPI_HOME,CUDA_HOME,NCCL_HOME)
+      #build command: $> make MPI=1 MPI_HOME=/path/to/mpi
+      #     CUDA_HOME=/path/to/cuda NCCL_HOME=/path/to/nccl
+      self.build_system.max_concurrency = 8
+      self.build_system.options=[MPI, "MPI_HOME="+MPI_HOME, "CUDA_HOME="+CUDA_HOME, "NCCL_HOME="+NCCL_HOME]
+      #self.build_system.max_concurrency = "{} MPI_HOME={} CUDA_HOME={} NCCL_HOME={}".format(MPI,MPI_HOME,CUDA_HOME,NCCL_HOME)
       # tests are located in $NCCL_HOME/build
 
    @sanity_function
@@ -138,13 +149,11 @@ class build_nccl_tests(rfm.CompileOnlyRegressionTest):
 
 
 
-
-
 class NCCLTestBase(rfm.RunOnlyRegressionTest):
    '''Base class of NCCL tests'''
 
    valid_systems = ['*']
-   valid_prog_environs = ['builtin']
+   valid_prog_environs = ['*']
    num_tasks = 1
    num_tasks_per_node = 1
    exclusive_access = True
@@ -156,12 +165,9 @@ class NCCLTestBase(rfm.RunOnlyRegressionTest):
 
 
 
-
 @rfm.simple_test
 class nccl_alltoall_test(NCCLTestBase):
    descr = 'NCCL All-to-All test'
-
-   outfiles = []
 
    @run_after('compile')
    def set_test_flags(self):
@@ -171,30 +177,28 @@ class nccl_alltoall_test(NCCLTestBase):
    @run_before('run')
    def prepare_run(self):
       # $> <nccl_test_dir>/build/alltoall_perf -b 8 -e 128M -f 2 -g 2
-      ncclt_test_dir = os.path.join(self.nccl_test.stagedir,
+      nccl_test_dir = os.path.join(self.nccl_test.stagedir,
             self.nccl_test.build_prefix, 'build' )
       self.executable = os.path.join(nccl_test_dir, 'alltoall')
       self.executable_opts = ['-b 8', '-e 128M', '-f 2', '-g 2']
 
    #@performance_function('GB/s')
    def get_bandwidth(self,filename):
-   #ireed: todo - find out how to fail after perf_function
-      if os.path.getsize(filename) == 0:
-         print("Error: Node pair {} failed".format(filename))
-         return False
       with open(str(filename), mode='r') as fp:
          data = fp.read()
       print(data)
-      data_list = data.split('\n')
-      bw = [s for s in data_list if '33554432' in s]
-      bw = re.split(' +', bw[0])
-      gbs = float(float(bw[1]) / 1000)
-      gbs = float(f'{gbs:.2f}')
-      print(gbs)
-      return gbs
+      return 0
+      #data_list = data.split('\n')
+      #bw = [s for s in data_list if '33554432' in s]
+      #bw = re.split(' +', bw[0])
+      #gbs = float(float(bw[1]) * 8 / 1000)
+      #gbs = float(f'{gbs:.2f}')
+      #print(gbs)
+      #return gbs
 
    @sanity_function
    def validate_test(self):
-      #get_bandwidth(self.stdout)
+   #ireed: todo - find out expected b/w outputs
+      gbs = self.get_bandwidth(self.stdout)
       sn.assert_ne(self.job.exitcode, 0, "Error NCCL test failed")
 
