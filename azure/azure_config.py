028e5f56fad96496f0f1968f5cd8499c47831594
Isayah Reed <isayah.reed@gmail.com>
2023-01-31 10:14:53 -0800

    Create azure_config.py
    Experimenting with a different config structure: including all vm_series as partitions of a single cluster system_name, instead of separate systems

---
+++ azure/azure_config.py
@@ -0,0 +1,108 @@
+
+site_configuration = {
+    'systems': [
+        {
+            'name': 'azure',
+            'descr': 'Azure VM',
+            'vm_data_file': 'azure_nhc/vm_info/azure_vms_dataset.json',
+            'hostnames': ['*'],
+            'modules_system': 'tmod4',
+            'partitions': [
+                {
+                    'name': 'hbv2',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'max_jobs': 100,
+                    #ireed: not needed
+                    'access': ['-p hbv2'],
+                    'environs': ['gnu-azhpc'],
+                    'prepare_cmds': ['source /etc/profile.d/modules.sh'],
+                    'features': ['ib', 'mpi'],
+                    'extras': {'vm_size': 'HB120rs_v2', 'gpu_arch': 'a100' }
+                },
+                {
+                    'name': 'nca100v4',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'max_jobs': 100,
+                    #ireed: not needed
+                    'access': ['-p nca100v4'],
+                    'environs': ['gnu-azhpc'],
+                    'prepare_cmds': ['source /etc/profile.d/modules.sh'],
+                    'features': ['ib', 'gpu', 'cuda', 'mpi'],
+                    'extras': {'vm_size': 'ncads_a100_v4', 'gpu_arch': 'a100' }
+                }
+            ]
+        },
+        {
+            'name': 'generic',
+            'descr': 'Generic example system',
+            'hostnames': ['.*'],
+            'partitions': [
+                {
+                    'name': 'default',
+                    'scheduler': 'local',
+                    'launcher': 'local',
+                    'environs': ['builtin'],
+                    'prepare_cmds': ['source /etc/profile.d/modules.sh']
+                }
+            ]
+        }
+    ],
+    'environments': [
+        {
+            'name': 'builtin',
+            'cc': 'cc',
+            'cxx': '',
+            'ftn': ''
+        },
+        {
+            'name': 'gnu-azhpc',
+            'modules': ['mpi/hpcx'],
+            'cc': 'gcc',
+            'cxx': 'g++',
+            'ftn': 'gfortran'
+        },
+        {
+            'name': 'gnu',
+            'cc': 'gcc',
+            'cxx': 'g++',
+            'ftn': 'gfortran'
+        },
+    ],
+    'logging': [
+        {
+            'handlers': [
+                {
+                    'type': 'stream',
+                    'name': 'stdout',
+                    'level': 'info',
+                    'format': '%(message)s'
+                },
+                {
+                    'type': 'file',
+                    'level': 'debug',
+                    'format': '[%(asctime)s] %(levelname)s: %(check_info)s: %(message)s',   # noqa: E501
+                    'append': False
+                }
+            ],
+            'handlers_perflog': [
+                {
+                    'type': 'filelog',
+                    'prefix': '%(check_system)s/%(check_partition)s',
+                    'level': 'info',
+                    'format': (
+                        '%(check_job_completion_time)s|reframe %(version)s|'
+                        '%(check_info)s|jobid=%(check_jobid)s|'
+                        '%(check_perf_var)s=%(check_perf_value)s|'
+                        'ref=%(check_perf_ref)s '
+                        '(l=%(check_perf_lower_thres)s, '
+                        'u=%(check_perf_upper_thres)s)|'
+                        '%(check_perf_unit)s'
+                    ),
+                    'append': True
+                }
+            ]
+        }
+    ],
+}
